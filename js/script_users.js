2a39cc3ff82ab11fbe5e1386922d90b456a0697b
Ivan Denysov <ivan.denysov.v@gmail.com>
2025-10-31 12:08:34 +0100

    Mise à jour des textes des boutons et ajout de la fonctionnalité d'affichage des utilisateurs dans les pages d'ajout, de modification et de suppression d'utilisateur.

--- js/script_users.js
+++ js/script_users.js
@@ -1,139 +1,129 @@
-    "use strict";
+"use strict";
 
 const URL = "http://fbrc.esy.es/DWWM22053/Api/api.php/users";
-  
-    let affiche_btn = document.getElementById("btn-afficher");
-    if (affiche_btn) {
-        affiche_btn.addEventListener("click", getListUsers);
-    }
 
-    let add_btn = document.getElementById("btn-add");
-    if (add_btn) {
-        add_btn.addEventListener("click", addUser);
-    }
+let affiche_btn = document.getElementById("btn-afficher");
+if (affiche_btn) {
+    affiche_btn.addEventListener("click", getListUsers);
+}
 
-    let change_btn = document.getElementById("btn-change");
-    if (change_btn) {
-        change_btn.addEventListener("click", changeUser);
-    }
+let add_btn = document.getElementById("btn-add");
+if (add_btn) {
+    add_btn.addEventListener("click", addUser);
+}
 
-    let del_btn = document.getElementById("btn-del");
-    if (del_btn) {
-        del_btn.addEventListener("click", delUser);
-    }
+let change_btn = document.getElementById("btn-change");
+if (change_btn) {
+    change_btn.addEventListener("click", changeUser);
+}
 
-    let del_input_id = document.getElementById("inp-del");
-    let affiche_table = document.getElementById("t-body");
-    let id_input = document.getElementById("id");
-    let nom_input = document.getElementById("nom");
-    let prenom_input = document.getElementById("prenom");
-    let email_input = document.getElementById("email");
+let del_btn = document.getElementById("btn-del");
+if (del_btn) {
+    del_btn.addEventListener("click", delUser);
+}
 
-    // MES FONCTIONS -------------------------
-    function getListUsers() {
-        let requestOptions = {
-            method: "GET" // par défaut
-        };
-        
-        fetch(URL, requestOptions)
-            .then((response) => response.json())
-            .then(function (data) {
-                console.log("La requête GET a abouti avec la réponse JSON : ", data.users.records);
-                for(let i in data.users.records){
-                    console.log(data.users.records[i]);
-                    affiche_table.innerHTML += `<tr class=""><td scope="row">${data.users.records[i][0]}</td><td scope="row">${data.users.records[i][1]}</td><td scope="row">${data.users.records[i][2]}</td><td scope="row">${data.users.records[i][3]}</td></tr>`
-                }
-            })
-            .catch(function (error) {
-                console.log("La requête GET a échoué : ", error);
-            });
-    }
-    
+let del_input_id = document.getElementById("inp-del");
+let affiche_table = document.getElementById("t-body");
+let id_input = document.getElementById("id");
+let nom_input = document.getElementById("nom");
+let prenom_input = document.getElementById("prenom");
+let email_input = document.getElementById("email");
 
-    function addUser() {
-        let requestOptions = {
-            method: "POST",
-            body: JSON.stringify({
-                id: parseInt(id_input.value),
-                nom: nom_input.value,
-                prenom: prenom_input.value,
-                email: email_input.value
-            })
-        };
+// MES FONCTIONS -------------------------
+function getListUsers() {
+    affiche_table.innerHTML = ""
 
-        fetch(URL, requestOptions)
-            .then((response) => response.text())
-            .then((data) => {
-                console.log("La requête POST a abouti avec la réponse JSON : ", data);
-                return data;
-            })
-            .catch((error) => {
-                console.log("La requête POST a échoué : ", error);
-                return -1;
-            });
-    }
-    
-    
-    // SUPPRIME UN user
+    let requestOptions = {
+        method: "GET" // par défaut
+    };
 
-    function delUser() {
-        affiche_table.innerHTML = ""
-        let requestOptions = {
-            method: "DELETE",
-        };
-
-        // fetch(URL + "/", requestOptions)
-        fetch(URL + "/" + del_input_id.value, requestOptions)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("La requête DELETE a abouti avec la réponse JSON : ", data);
-                del_input_id.value = "";
-                getListUsers();
-            })
-            .catch((error) => {
-                console.log("La requête DELETE a échoué : ", error);
-            });
-
-    }
+    fetch(URL, requestOptions)
+        .then((response) => response.json())
+        .then(function (data) {
+            console.log("La requête GET a abouti avec la réponse JSON : ", data.users.records);
+            for (let i in data.users.records) {
+                console.log(data.users.records[i]);
+                affiche_table.innerHTML += `<tr class=""><td scope="row">${data.users.records[i][0]}</td><td scope="row">${data.users.records[i][1]}</td><td scope="row">${data.users.records[i][2]}</td><td scope="row">${data.users.records[i][3]}</td></tr>`
+            }
+        })
+        .catch(function (error) {
+            console.log("La requête GET a échoué : ", error);
+        });
+}
 
 
-    // MODIFIE UNE user
-    // let userMAJ = {
-    //     marque: "Peugeot 208",
-    //     couleur:"rouge",
-    //     cylindree: 90
-    // };
-    
-    // let requestOptions = {
-    //     method: "PUT",
-    //     body: JSON.stringify(userMAJ)
-    // };
+function addUser() {
+    affiche_table.innerHTML = ""
+    let requestOptions = {
+        method: "POST",
+        body: JSON.stringify({
+            id: parseInt(id_input.value),
+            nom: nom_input.value,
+            prenom: prenom_input.value,
+            email: email_input.value
+        })
+    };
 
-    // fetch(URL + "/100000057", requestOptions)
-    //     .then((response) => response.text())
-    //     .then(function (data) {
-    //         console.log("La requête PUT a abouti avec la réponse JSON : ", data);
-    //     })
-    //     .catch(function (error) {
-    //         console.log("La requête PUT a échoué : ", error);
-    //     });
+    fetch(URL, requestOptions)
+        .then((response) => response.text())
+        .then((data) => {
+            getListUsers();
+            console.log("La requête POST a abouti avec la réponse JSON : ", data);
+            return data;
+        })
+        .catch((error) => {
+            console.log("La requête POST a échoué : ", error);
+            return -1;
+        });
+}
 
 
-// for(let i = 100000015; i < 100000052; i++) {
-//     del(i);
-// }
+// SUPPRIME UN user
 
-// function del(iduser) {
-//     requestOptions = {
-//         method: "DELETE",
-//     };
+function delUser() {
+    affiche_table.innerHTML = ""
+    let requestOptions = {
+        method: "DELETE",
+    };
 
-//     fetch(URL + "/" + iduser, requestOptions)
-//         .then((response) => response.text())
-//         .then((data) => {
-//             console.log("La requête DELETE a abouti avec la réponse JSON : ", data);
-//         })
-//         .catch((error) => {
-//             console.log("La requête DELETE a échoué : ", error);
-//         });
-// }
+    // fetch(URL + "/", requestOptions)
+    fetch(URL + "/" + del_input_id.value, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("La requête DELETE a abouti avec la réponse JSON : ", data);
+            del_input_id.value = "";
+            getListUsers();
+        })
+        .catch((error) => {
+            console.log("La requête DELETE a échoué : ", error);
+        });
+
+}
+
+
+// MODIFIE UNE user
+
+function changeUser() {
+    affiche_table.innerHTML = ""
+
+    let requestOptions = {
+        method: "PUT",
+        body: JSON.stringify({
+            id: parseInt(id_input.value),
+            nom: nom_input.value,
+            prenom: prenom_input.value,
+            email: email_input.value
+        })
+    };
+
+    fetch(URL + "/" + parseInt(id_input.value), requestOptions)
+        .then((response) => response.text())
+        .then(function (data) {
+            console.log("La requête PUT a abouti avec la réponse JSON : ", data);
+            affiche_table.innerHTML = ""
+            getListUsers();
+        })
+        .catch(function (error) {
+            console.log("La requête PUT a échoué : ", error);
+        });
+}
